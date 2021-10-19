ae15d94a1710b0e1e74054da1f9798c609973c16
Kiishi201 <68674972+Kiishi201@users.noreply.github.com>
2021-10-18 22:36:50 -0400

    error in RepairDiagramsFeature.java

--- app/src/main/java/com/example/clsmandroidapp/RepairDiagramsFeature.java
+++ app/src/main/java/com/example/clsmandroidapp/RepairDiagramsFeature.java
@@ -3,12 +3,39 @@ package com.example.clsmandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.GridLayout;
+import java.util.ArrayList;
+import android.view.Menu;
+import android.app.Activity;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+
+
+
+
+
+import com.example.clsmandroidapp.databinding.ActivityFeatureSelectionBinding;
+
+
+
 
 public class RepairDiagramsFeature extends AppCompatActivity {
+    GridView repair_diagram_feature;
+
+
+    private ActivityFeatureSelectionBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repair_diagrams_feature);
+
+        GridView repair_diagram_feature = (GridView) findViewById(R.id.repair_diagram_feature);
+        repair_diagram_feature.setAdapter(new ImageAdapter(this));
+
     }
 }
