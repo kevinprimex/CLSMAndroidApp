78907d559432df2b5921e395b2a4e739eec557b9
Josh Dhori <Joshbr32@gmail.com>
2021-10-18 21:29:31 -0400

    Added a template for RepairDiagramsFeature for Kiishi
    Added contentScrolling feature

---
+++ app/src/main/java/com/example/clsmandroidapp/RepairDiagramsFeature.java
@@ -0,0 +1,14 @@
+package com.example.clsmandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RepairDiagramsFeature extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repair_diagrams_feature);
+    }
+}
